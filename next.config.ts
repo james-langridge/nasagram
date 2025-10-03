be7d79ccc501d19be6ba2ceb7482a0efbf59fdf5
James Langridge <l@ngridge.com>
2025-10-03 21:55:03 +0200

    Add mars.jpl.nasa.gov to allowed image hosts
    Curiosity rover uses mars.jpl.nasa.gov for images, which was not
    in the Next.js remotePatterns config, causing 400 errors.
    
    Image hosts by rover:
    - Spirit/Opportunity: mars.nasa.gov
    - Curiosity: mars.jpl.nasa.gov
    - Perseverance: mars.nasa.gov
    
    Added mars.jpl.nasa.gov to fix Curiosity image loading.

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,11 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "mars.jpl.nasa.gov",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
         hostname: "lh3.googleusercontent.com",
         pathname: "/**",
       },
